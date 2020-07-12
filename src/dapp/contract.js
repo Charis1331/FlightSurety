7c32d97daea9b2349295202cd193e94696900bf7
houlisha <h.houlis@afse.eu>
2020-07-12 15:19:22 +0300

    Initial commit

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -9,14 +9,14 @@ export default class Contract {
         this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
         this.initialize(callback);
         this.owner = null;
-        this.airlines = ['AirAB1', 'AirAB2', 'AirAB3', 'AirAB4', 'AirAB5'];
-        this.passengers = ['PAB1', 'PAB2', 'PAb3', 'PAB4', 'PAb5'];
+        this.airlines = ['Air1', 'Air2', 'Air3', 'Air4', 'Air5'];
+        this.passengers = ['PAS1', 'PAS2', 'PAS3', 'PAS4', 'PAS5'];
         this.flights = {
-            'AirAB1': ['FL1', 'FL2', 'FL3'],
-            'AirAB2': ['AL1', 'AL2', 'AL3'],
-            'AirAB3': ['BL1', 'BL2', 'BL3'],
-            'AirAB4': ['CL1', 'CL2', 'CL3'],
-            'AirAB5': ['DL1', 'DL2', 'DL3'],
+            'Air1': ['FL1', 'FL2', 'FL3'],
+            'Air2': ['AL1', 'AL2', 'AL3'],
+            'Air3': ['BL1', 'BL2', 'BL3'],
+            'Air4': ['CL1', 'CL2', 'CL3'],
+            'Air5': ['DL1', 'DL2', 'DL3'],
         }
     }
 
@@ -70,7 +70,7 @@ export default class Contract {
 
     sendFunds(airline, funds, callback) {
         let self = this;
-        
+
         const fundsToSend = self.web3.utils.toWei(funds, "ether");
         console.log(fundsToSend);
         self.flightSuretyApp.methods
@@ -87,7 +87,7 @@ export default class Contract {
         const fundstosend1 = self.web3.utils.toWei(funds_ether, "ether");
         console.log(fundstosend1);
         //console.log("passenger buy:" + )
-        let ts = timestamp;//1553367808;
+        let ts = timestamp; //1553367808;
         self.flightSuretyApp.methods
             .buyInsurance(airline.toString(), flight.toString(), ts)
             .send({ from: passenger.toString(), value: fundstosend1, gas: 1000 }, (error, result) => {
@@ -109,8 +109,8 @@ export default class Contract {
         let self = this;
 
         self.web3.eth.
-            getBalance(passenger1).call({ from: passenger }, (error, result) => {
-                callback(error, result);
-            });
+        getBalance(passenger1).call({ from: passenger }, (error, result) => {
+            callback(error, result);
+        });
     }
 }
